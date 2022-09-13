302137e9d0010b8334cbee68fb8f4d5ed61750c6
c8ef <c8ef@outlook.com>
2022-09-13 23:03:05 +0800

    3 tests not pass, the caltrain is still buggy😖

--- P3/party.cc
+++ P3/party.cc
@@ -1,5 +1,7 @@
 #include <condition_variable>
 #include <mutex>
+#include <queue>
+#include <string>
 
 // The total number of Zodiac signs
 static const int NUM_SIGNS = 12;
@@ -13,12 +15,23 @@ public:
 
 private:
   // Synchronizes access to this structure.
-  std::mutex mutex;
+  std::mutex mutex_;
+  std::queue<std::string> party_map_[12][12]{};
+  std::condition_variable cond_map_[12][12]{};
 };
 
-Party::Party() : mutex() {}
+Party::Party() : mutex_() {}
 
 std::string Party::meet(std::string &my_name, int my_sign, int other_sign) {
-  // You need to implement this
-  return "??";
+  std::unique_lock lock(mutex_);
+  if (my_sign != other_sign) {
+    party_map_[my_sign][other_sign].push(my_name);
+    cond_map_[my_sign][other_sign].notify_one();
+  }
+
+  while (my_sign != other_sign && party_map_[other_sign][my_sign].size() == 0)
+    cond_map_[other_sign][my_sign].wait(lock);
+  std::string ret = party_map_[other_sign][my_sign].front();
+  party_map_[other_sign][my_sign].pop();
+  return ret;
 }
