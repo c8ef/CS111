97935d77114750924b85ce2c848ac13fd6e1d75f
c8ef <c8ef@outlook.com>
2022-09-14 21:10:28 +0800

    simplify a little bit, but still a test case left

--- P3/party.cc
+++ P3/party.cc
@@ -10,39 +10,25 @@ static const int NUM_SIGNS = 12;
 // to their zodiac signs.
 class Party {
 public:
-  Party();
+  Party() = default;
   std::string meet(std::string &my_name, int my_sign, int other_sign);
 
 private:
   // Synchronizes access to this structure.
-  std::mutex mutex_;
-  std::queue<std::string> party_map_[12][12]{};
-  std::condition_variable cond_map_[12][12]{};
-
+  std::mutex mutex_{};
+  std::queue<std::string> party_map_[25][25]{};
+  std::condition_variable cond_map_[25][25]{};
   int same_index_[12]{};
-  std::queue<std::string> same_party_map_[13][13]{};
-  std::condition_variable same_cond_map_[13][13]{};
 };
-
-Party::Party() : mutex_() {}
 
 std::string Party::meet(std::string &my_name, int my_sign, int other_sign) {
   std::unique_lock lock(mutex_);
   if (my_sign == other_sign) {
     ++same_index_[my_sign];
     if (same_index_[my_sign] % 2 == 0)
-      ++other_sign;
+      other_sign += 12;
     else
-      ++my_sign;
-
-    same_party_map_[my_sign][other_sign].push(my_name);
-    same_cond_map_[my_sign][other_sign].notify_one();
-
-    while (same_party_map_[other_sign][my_sign].size() == 0)
-      same_cond_map_[other_sign][my_sign].wait(lock);
-    std::string ret = same_party_map_[other_sign][my_sign].front();
-    same_party_map_[other_sign][my_sign].pop();
-    return ret;
+      my_sign += 12;
   }
 
   party_map_[my_sign][other_sign].push(my_name);
