d08296f09c6f217a4b25519e7a7ffe4c71bb9494
c8ef <c8ef@outlook.com>
2022-09-15 00:01:20 +0800

    closer

--- P3/party.cc
+++ P3/party.cc
@@ -16,8 +16,11 @@ public:
 private:
   // Synchronizes access to this structure.
   std::mutex mutex_{};
+  int timer_{};
   std::queue<std::string> party_map_[2 * NUM_SIGNS + 1][2 * NUM_SIGNS + 1]{};
   std::condition_variable cond_map_[2 * NUM_SIGNS + 1][2 * NUM_SIGNS + 1]{};
+  std::priority_queue<int, std::vector<int>, std::greater<int>>
+      min_time_[2 * NUM_SIGNS + 1][2 * NUM_SIGNS + 1]{};
   int same_index_[NUM_SIGNS]{};
 };
 
@@ -31,12 +34,18 @@ std::string Party::meet(std::string &my_name, int my_sign, int other_sign) {
       my_sign += NUM_SIGNS;
   }
 
+  int curr_time = timer_;
+  timer_++;
+  min_time_[other_sign][my_sign].push(curr_time);
+
   party_map_[my_sign][other_sign].push(my_name);
   cond_map_[my_sign][other_sign].notify_one();
 
-  while (party_map_[other_sign][my_sign].size() == 0)
+  while (party_map_[other_sign][my_sign].size() == 0 ||
+         curr_time != min_time_[other_sign][my_sign].top())
     cond_map_[other_sign][my_sign].wait(lock);
   std::string ret = party_map_[other_sign][my_sign].front();
   party_map_[other_sign][my_sign].pop();
+  min_time_[other_sign][my_sign].pop();
   return ret;
 }
