4015f690ccd4281f4c47d7109891d81a3180afcb
c8ef <c8ef@outlook.com>
2022-09-14 11:39:06 +0800

    remove redundant condition

--- P3/party.cc
+++ P3/party.cc
@@ -45,12 +45,10 @@ std::string Party::meet(std::string &my_name, int my_sign, int other_sign) {
     return ret;
   }
 
-  if (my_sign != other_sign) {
-    party_map_[my_sign][other_sign].push(my_name);
-    cond_map_[my_sign][other_sign].notify_one();
-  }
+  party_map_[my_sign][other_sign].push(my_name);
+  cond_map_[my_sign][other_sign].notify_one();
 
-  while (my_sign != other_sign && party_map_[other_sign][my_sign].size() == 0)
+  while (party_map_[other_sign][my_sign].size() == 0)
     cond_map_[other_sign][my_sign].wait(lock);
   std::string ret = party_map_[other_sign][my_sign].front();
   party_map_[other_sign][my_sign].pop();
