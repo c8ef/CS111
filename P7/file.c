699d8ae11f422aaed0bada04d6dee881f4d96280
c8ef <c8ef@outlook.com>
2022-09-19 11:25:38 +0800

    pass the inode level functionality tests

--- P7/file.c
+++ P7/file.c
@@ -7,10 +7,13 @@
 
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum,
                   void *buf) {
-  // Remove the following placeholder code and add your implementation
-  fprintf(stderr,
-          "file_getblock(inumber = %d, blockNum = %d) "
-          "unimplemented. returning -1\n",
-          inumber, blockNum);
-  return -1;
+  struct inode i_node;
+  inode_iget(fs, inumber, &i_node);
+  int phy_block = inode_indexlookup(fs, &i_node, blockNum);
+  int fize_size = inode_getsize(&i_node);
+  int ret = fize_size / DISKIMG_SECTOR_SIZE == blockNum
+                ? fize_size % DISKIMG_SECTOR_SIZE
+                : DISKIMG_SECTOR_SIZE;
+  diskimg_readsector(fs->dfd, phy_block, buf);
+  return ret;
 }
--- P7/inode.c
+++ P7/inode.c
@@ -1,28 +1,46 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "diskimg.h"
 #include "inode.h"
-#include <stdbool.h>
 
 #define INODES_PER_BLOCK (DISKIMG_SECTOR_SIZE / sizeof(struct inode))
 #define NUM_BLOCK_NUMS_PER_BLOCK (DISKIMG_SECTOR_SIZE / sizeof(uint16_t))
 
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-  // Remove the placeholder code below and add your implementation.
-  fprintf(stderr, "inode_get(inumber=%d) not implemented, returning -1\n",
-          inumber);
-  return -1;
+  int sector_num = (inumber - 1) / INODES_PER_BLOCK + 2,
+      in_sec_num = (inumber - 1) % INODES_PER_BLOCK;
+
+  struct inode inode_arr[INODES_PER_BLOCK];
+  diskimg_readsector(fs->dfd, sector_num, inode_arr);
+  *inp = inode_arr[in_sec_num];
+
+  return 0;
 }
 
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp,
                       int blockNum) {
-  // Remove the placeholder code below and add your implementation.
-  fprintf(stderr,
-          "inode_indexlookup(blockNum=%d) not implemented, "
-          "returning -1\n",
-          blockNum);
-  return -1;
+  // use i_mode detect large file
+  if (!(inp->i_mode & ILARG))
+    return inp->i_addr[blockNum] != 0 ? inp->i_addr[blockNum] : -1;
+  uint16_t buf[NUM_BLOCK_NUMS_PER_BLOCK];
+  if (blockNum < 7 * NUM_BLOCK_NUMS_PER_BLOCK) {
+    int indirBlockNum = blockNum / NUM_BLOCK_NUMS_PER_BLOCK;
+    diskimg_readsector(fs->dfd, inp->i_addr[indirBlockNum], buf);
+    return buf[blockNum % NUM_BLOCK_NUMS_PER_BLOCK] != 0
+               ? buf[blockNum % NUM_BLOCK_NUMS_PER_BLOCK]
+               : -1;
+  } else {
+    int indirBlockNum =
+        (blockNum - 7 * NUM_BLOCK_NUMS_PER_BLOCK) / NUM_BLOCK_NUMS_PER_BLOCK;
+    diskimg_readsector(fs->dfd, inp->i_addr[7], buf);
+    diskimg_readsector(fs->dfd, buf[indirBlockNum], buf);
+    return buf[blockNum % NUM_BLOCK_NUMS_PER_BLOCK] != 0
+               ? buf[blockNum % NUM_BLOCK_NUMS_PER_BLOCK]
+               : -1;
+  }
 }
 
 int inode_getsize(struct inode *inp) {
