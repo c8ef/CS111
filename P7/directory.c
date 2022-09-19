16d43e754b9663c71c0b8b9284a5d2b8be4bee1e
c8ef <c8ef@outlook.com>
2022-09-19 14:32:39 +0800

    finish P7

--- P7/directory.c
+++ P7/directory.c
@@ -1,4 +1,5 @@
 #include "directory.h"
+#include "direntv6.h"
 #include "diskimg.h"
 #include "file.h"
 #include "inode.h"
@@ -11,9 +12,28 @@
 
 int directory_findname(struct unixfilesystem *fs, const char *name,
                        int dirinumber, struct direntv6 *dirEnt) {
-  // Remove the following placeholder implementation and replace
-  // with a real implementation.
-  fprintf(stderr, "directory_lookupname(name=%s dirinumber=%d)\n", name,
-          dirinumber);
+  struct inode ino;
+  if (inode_iget(fs, dirinumber, &ino) < 0)
+    return -1;
+  if ((ino.i_mode & IFDIR) == 0)
+    return -1;
+  int dir_size;
+  if ((dir_size = inode_getsize(&ino)) <= 0)
+    return -1;
+
+  int total_block_num = (dir_size - 1) / DISKIMG_SECTOR_SIZE + 1;
+  for (int i = 0; i < total_block_num; i++) {
+    struct direntv6 entries[NUM_DIRENTS_PER_BLOCK];
+    int valid_bytes = file_getblock(fs, dirinumber, i, entries);
+    if (valid_bytes < 0)
+      return -1;
+    int total_entry_num = valid_bytes / sizeof(struct direntv6);
+    for (int j = 0; j < total_entry_num; j++) {
+      if (strncmp(entries[j].d_name, name, MAX_COMPONENT_LENGTH) == 0) {
+        *dirEnt = entries[j];
+        return 0;
+      }
+    }
+  }
   return -1;
 }
--- P7/pathname.c
+++ P7/pathname.c
@@ -1,4 +1,3 @@
-
 #include "pathname.h"
 #include "directory.h"
 #include "diskimg.h"
@@ -8,8 +7,24 @@
 #include <string.h>
 
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-  // Remove the placeholder code below and add your implementation.
-  fprintf(stderr, "pathname_lookup(path=%s) unimplemented.  Returing -1.\n",
-          pathname);
-  return -1;
+  if (strcmp(pathname, "/") == 0)
+    return 1;
+  struct direntv6 dir;
+  char *p = pathname + 1, *q = pathname + 1;
+  int dir_num = 1;
+  while (1) {
+    while (*q && *q != '/')
+      q++;
+    char dir_buf[14] = "";
+    strncpy(dir_buf, p, q - p);
+
+    if (directory_findname(fs, dir_buf, dir_num, &dir) == -1)
+      return -1;
+    if (!*q) {
+      return dir.d_inumber;
+    }
+    p = q + 1;
+    q = p;
+    dir_num = dir.d_inumber;
+  }
 }
